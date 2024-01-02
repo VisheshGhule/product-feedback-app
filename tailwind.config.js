b34906c70a32d83a42dadc8cc0ce5a09b8c57b87
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-02 12:04:43 +0000

    setup project font-size and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,74 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
+    fontFamily: {
+      sans: ["Jost", "Arial", "Helvetica", "sans-serif"],
+    },
+    fontSize: {
+      s1: [
+        "13px",
+        {
+          lineHeight: "19px",
+          fontWeight: "400",
+        },
+      ],
+      s2: [
+        "14px",
+        {
+          lineHeight: "20px",
+          letterSpacing: "-0.25",
+          fontWeight: "700",
+        },
+      ],
+      s3: [
+        "15px",
+        {
+          lineHeight: "22px",
+          fontWeight: "400",
+        },
+      ],
+      s4: [
+        "16px",
+        {
+          lineHeight: "23px",
+          fontWeight: "400",
+        },
+      ],
+      s5: [
+        "18px",
+        {
+          lineHeight: "26px",
+          letterSpacing: "-0.25",
+          fontWeight: "700",
+        },
+      ],
+      s6: [
+        "20px",
+        {
+          lineHeight: "29px",
+          letterSpacing: "-0.25",
+          fontWeight: "700",
+        },
+      ],
+      " ": [
+        "24px",
+        {
+          lineHeight: "35px",
+          letterSpacing: "-0.33",
+          fontWeight: 700,
+        },
+      ],
+    },
+    colors: {
+      "rich-purple": "#AD1FEA",
+      "royal-blue": "#4661E6",
+      "lightest-gray": "#F2F4FF",
+      "silver-cloud": "#F7F8FD",
+      "deep-indigo": "#3A4374",
+      "dusk-blue": "#647196",
+      peach: "#F49F85",
+      "bright-teal": "#62BCFA",
+    },
     extend: {},
   },
   plugins: [require("flowbite/plugin")],
