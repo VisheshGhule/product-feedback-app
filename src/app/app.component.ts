1e7cd7029cd916dcca57288186d4c203a4e1f255
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-03 14:56:33 +0000

    create tag button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,18 @@ import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { ButtonComponent } from './components/buttons/button/button.component';
 import { GoBackButtonComponent } from './components/buttons/go-back-button/go-back-button.component';
+import { TagButtonComponent } from './components/buttons/tag-button/tag-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonComponent, GoBackButtonComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ButtonComponent,
+    GoBackButtonComponent,
+    TagButtonComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/components/buttons/tag-button/tag-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './tag-button.component.html',
+})
+export class TagButtonComponent {}
