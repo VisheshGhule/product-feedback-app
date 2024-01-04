ee06ed06c765a325548fb01347b734fa73af1b50
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-04 17:00:35 +0000

    remove unsed import

--- src/app/components/buttons/vote-button/vote-button.component.ts
+++ src/app/components/buttons/vote-button/vote-button.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { IconComponent } from '../../icon/icon.component';
 
 @Component({
   selector: 'app-vote-button',
   standalone: true,
-  imports: [MatIconModule, IconComponent],
+  imports: [MatIconModule],
   templateUrl: './vote-button.component.html',
 })
 export class VoteButtonComponent {}
