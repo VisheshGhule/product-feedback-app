5ad64bda0d8a38aea3396a1e8cb16b0c53e45b01
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-05 17:00:10 +0000

    refactor button component template

--- src/app/components/buttons/button/button.component.ts
+++ src/app/components/buttons/button/button.component.ts
@@ -4,10 +4,19 @@ import { Component, Input } from '@angular/core';
   selector: 'app-button',
   standalone: true,
   imports: [],
-  templateUrl: './button.component.html',
+  template: `
+    <button
+      type="submit"
+      [class]="
+        'text-s1 text-lightest-gray w-full h-[44px] rounded-lg hover:bg-opacity-70 transition-colors '.concat(
+          customClass
+        )
+      "
+    >
+      <ng-content></ng-content>
+    </button>
+  `,
 })
 export class ButtonComponent {
-  @Input() text = 'Button';
   @Input() customClass = '';
-  defaultClass = 'text-s1 text-lightest-gray w-full h-[44px] rounded-lg hover:bg-opacity-70 transition-colors';
 }
