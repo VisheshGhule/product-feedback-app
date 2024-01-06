1ab1ee202c1df4ae47a83051426de8789416cceb
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-06 10:37:57 +0000

    include background color style property

--- src/app/components/buttons/button/button.component.ts
+++ src/app/components/buttons/button/button.component.ts
@@ -5,19 +5,24 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-      <button
-        [type]="buttonType || 'button'"
-        [class]="
-          'text-s1 text-lightest-gray px-4 w-full h-[44px] rounded-custom-lg hover:bg-opacity-70 transition-colors text-center '.concat(
-            customClass
-          )
-        "
-      >
-        <ng-content></ng-content>
-      </button>
+    <button
+      [type]="buttonType || 'button'"
+      [class]="
+        'text-s1 text-lightest-gray px-4 w-full h-[44px] rounded-custom-lg hover:bg-opacity-70 transition-colors text-center '
+          .concat('bg-')
+          .concat(bgColor)
+      "
+    >
+      <ng-content></ng-content>
+    </button>
   `,
 })
 export class ButtonComponent {
   @Input() customClass = '';
   @Input() buttonType = '';
+  @Input() bgColor!:
+    | 'rich-purple'
+    | 'royal-blue'
+    | 'deep-indigo'
+    | 'brick-red';
 }
