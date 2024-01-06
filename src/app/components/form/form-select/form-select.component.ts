1378f3d9a5a8599cb6c6f6f5d94c437c071005ed
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-06 00:56:30 +0000

    create a custom select component

---
+++ src/app/components/form/form-select/form-select.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { FormLabelComponent } from '../form-label/form-label.component';
+
+@Component({
+  selector: 'app-form-select',
+  standalone: true,
+  imports: [MatIconModule, FormLabelComponent],
+  templateUrl: './form-select.component.html',
+})
+export class FormSelectComponent implements OnInit {
+  @Input() options: Array<{ name: string; value: string }> = [];
+  @Input() selectId!: string;
+  selectedItem = '';
+
+  ngOnInit(): void {
+    if (this.options.length > 0) {
+      this.selectedItem = this.options[0].name;
+    }
+  }
+
+  handleSelection(index: number): void {
+    this.selectedItem = this.options[index].name;
+  }
+
+  preventDefault(event: Event): void {
+    event.preventDefault();
+  }
+}
