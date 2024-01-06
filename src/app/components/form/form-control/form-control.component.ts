236c4012bdb59213a0c3cdcd6f244c9703c29a62
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-06 00:57:11 +0000

    create a component for form controls

---
+++ src/app/components/form/form-control/form-control.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control',
+  standalone: true,
+  imports: [],
+  template: `<ng-content></ng-content>`,
+})
+export class FormControlComponent {}
