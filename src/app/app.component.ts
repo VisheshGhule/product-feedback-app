6ba6b116a5da1751f4644c41162568f9df67a355
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-11 11:33:06 +0000

    feat: move homepage markup to homepage component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,76 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-
-import { ButtonComponent } from './components/buttons/button/button.component';
-import { GoBackButtonComponent } from './components/buttons/go-back-button/go-back-button.component';
-import { TagButtonComponent } from './components/buttons/tag-button/tag-button.component';
-import { VoteButtonComponent } from './components/buttons/vote-button/vote-button.component';
-import { SortMenuComponent } from './components/sort-menu/sort-menu.component';
-import { FormInputComponent } from './components/form/form-input/form-input.component';
-import { FormLabelComponent } from './components/form/form-label/form-label.component';
-import { FormSelectComponent } from './components/form/form-select/form-select.component';
-import { FormControlComponent } from './components/form/form-control/form-control.component';
-import { FormTextareaComponent } from './components/form/form-textarea/form-textarea.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatIconModule,
-    RouterOutlet,
-    ButtonComponent,
-    GoBackButtonComponent,
-    TagButtonComponent,
-    VoteButtonComponent,
-    SortMenuComponent,
-    FormInputComponent,
-    FormLabelComponent,
-    FormSelectComponent,
-    FormControlComponent,
-    FormTextareaComponent
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'product-feedback-app';
-  selectOptions = [
-    {
-      name: 'Mr.',
-      value: 'mr',
-    },
-    {
-      name: 'Mrs',
-      value: 'nrs',
-    },
-    {
-      name: 'Miss.',
-      value: 'miss',
-    },
-    {
-      name: 'Dr.',
-      value: 'dr',
-    },
-  ];
-  menuOptions = [
-    {
-      name: 'Most Upvotes',
-      href: 'most-upvotes',
-    },
-    {
-      name: 'Least Upvotes',
-      href: 'least-upvotes',
-    },
-    {
-      name: 'Most Comments',
-      href: 'most-comments',
-    },
-    {
-      name: 'Least Comments',
-      href: 'least-comments',
-    },
-  ];
-}
+export class AppComponent {}
