111ebd3e3eab4240a91759894e8a08c48e7795df
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-11 12:06:58 +0000

    feat: add ui markup

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
+import { TagButtonComponent } from '../../components/buttons/tag-button/tag-button.component';
+import { ButtonComponent } from '../../components/buttons/button/button.component';
+import { SortMenuComponent } from '../../components/sort-menu/sort-menu.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    TagButtonComponent,
+    ButtonComponent,
+    SortMenuComponent,
+    MatIconModule,
+  ],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
