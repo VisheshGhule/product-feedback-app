cabd33e8590abf738432e85be7c7b5fb63dfe99d
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-11 11:35:08 +0000

    feat: create homepage and new feedback page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NewFeedbackPageComponent } from './pages/new-feedback-page/new-feedback-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'new-feedback', component: NewFeedbackPageComponent },
+];
