aaff6db167987f5c243ae5f25628cb3ed08a1fc3
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-12 07:48:59 +0000

    feat: create edit feedback page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { NewFeedbackPageComponent } from './pages/new-feedback-page/new-feedback-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { EditFeedbackPageComponent } from './pages/edit-feedback-page/edit-feedback-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'new-feedback', component: NewFeedbackPageComponent },
+  { path: 'edit-feedback', component: EditFeedbackPageComponent },
 ];
