f9abb806ac9ad32a60f71184c23901310ea123e3
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-12 07:50:26 +0000

    feat: create edit feedback page components

---
+++ src/app/pages/edit-feedback-page/edit-feedback-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-feedback-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-feedback-page.component.html',
+  styleUrl: './edit-feedback-page.component.css'
+})
+export class EditFeedbackPageComponent {
+
+}
