24eab958e9f6485c86e92ee98e28aa41d9920840
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-13 15:44:11 +0000

    feat: import feedback sample json data

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -5,6 +5,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { TagButtonComponent } from '../../components/buttons/tag-button/tag-button.component';
 import { ButtonComponent } from '../../components/buttons/button/button.component';
 import { SortMenuComponent } from '../../components/sort-menu/sort-menu.component';
+import { ProductRequest, User } from '../../model/product';
+import * as data from '../../../assets/data/data.json';
+
+interface ProductFeedbackData {
+  currentUser: User;
+  productRequests: ProductRequest[];
+}
 
 @Component({
   selector: 'app-home-page',
@@ -18,9 +25,14 @@ import { SortMenuComponent } from '../../components/sort-menu/sort-menu.componen
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
-  constructor(private _router: Router) {}
+  productRequests: ProductRequest[] = [];
+  productFeedbackData!: ProductFeedbackData;
 
-  newFeedBack(): void {
+  constructor(private _router: Router) {
+    this.productFeedbackData = data as ProductFeedbackData;
+  }
+
+  goTonewFeedBackPage(): void {
     this._router.navigateByUrl('/new-feedback');
   }
 }
