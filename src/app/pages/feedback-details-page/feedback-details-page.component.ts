6c7de3274ddc630b48ed7170a80d014f2a345923
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-14 23:21:46 +0000

    feat: select a feedback sample data with comments and replies

--- src/app/pages/feedback-details-page/feedback-details-page.component.ts
+++ src/app/pages/feedback-details-page/feedback-details-page.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as data from '../../../assets/data/data.json';
+import { ProductRequest, User } from '../../model/product';
+
+interface ProductFeedbackData {
+  currentUser: User;
+  productRequests: ProductRequest[];
+}
 
 @Component({
   selector: 'app-feedback-details-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './feedback-details-page.component.html',
 })
 export class FeedbackDetailsPageComponent {
+  feedback!: ProductRequest;
 
+  constructor() {
+    const productFeedbackData = data as ProductFeedbackData;
+    this.feedback = productFeedbackData.productRequests[1];
+  }
 }
