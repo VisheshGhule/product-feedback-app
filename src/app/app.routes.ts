755ffa775687fa48059824c301b6da3f3234e7fe
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-14 23:22:10 +0000

    feat: add feedback-details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { NewFeedbackPageComponent } from './pages/new-feedback-page/new-feedback-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { EditFeedbackPageComponent } from './pages/edit-feedback-page/edit-feedback-page.component';
+import { FeedbackDetailsPageComponent } from './pages/feedback-details-page/feedback-details-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'new-feedback', component: NewFeedbackPageComponent },
   { path: 'edit-feedback', component: EditFeedbackPageComponent },
+  { path: 'feedback-details', component: FeedbackDetailsPageComponent },
 ];
