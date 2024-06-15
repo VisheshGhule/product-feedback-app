11c088db58b339465e272769dd511d75a6b8eae9
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-15 20:00:27 +0000

    feat: create roadmap page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { NewFeedbackPageComponent } from './pages/new-feedback-page/new-feedback
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { EditFeedbackPageComponent } from './pages/edit-feedback-page/edit-feedback-page.component';
 import { FeedbackDetailsPageComponent } from './pages/feedback-details-page/feedback-details-page.component';
+import { RoadmapPageComponent } from './pages/roadmap-page/roadmap-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'new-feedback', component: NewFeedbackPageComponent },
   { path: 'edit-feedback', component: EditFeedbackPageComponent },
   { path: 'feedback-details', component: FeedbackDetailsPageComponent },
+  { path: 'roadmap', component: RoadmapPageComponent },
 ];
---
+++ src/app/pages/roadmap-page/roadmap-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roadmap-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './roadmap-page.component.html',
+})
+export class RoadmapPageComponent {}
