66853f5147427eefdf6b0c5a5f17feb5b7f778b3
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-17 12:42:59 +0000

    feat: implement show feedbacks by status selection

--- src/app/pages/roadmap-page/roadmap-page.component.ts
+++ src/app/pages/roadmap-page/roadmap-page.component.ts
@@ -1,9 +1,63 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+
+import { User } from './../../model/product';
+import { ProductRequest } from '../../model/product';
+import * as data from '../../../assets/data/data.json';
+
+interface ProductFeedbackData {
+  currentUser: User;
+  productRequests: ProductRequest[];
+}
+
+type ProductFeedbackStatus = 'suggestion' | 'planned' | 'in-progress' | 'live';
 
 @Component({
   selector: 'app-roadmap-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './roadmap-page.component.html',
 })
-export class RoadmapPageComponent {}
+export class RoadmapPageComponent {
+  productFeedbackData!: ProductFeedbackData;
+  productFeedbacks!: ProductRequest[];
+  selectedFeedbackStatus = new BehaviorSubject<ProductFeedbackStatus>(
+    'planned'
+  );
+
+  constructor() {
+    const productFeedbackData = data as ProductFeedbackData;
+    this.productFeedbackData = productFeedbackData;
+    this.productFeedbacks = this.filterFeedbacksByStatus('planned');
+  }
+
+  filterFeedbacksByStatus(
+    status: ProductFeedbackStatus | null
+  ): ProductRequest[] {
+    return this.productFeedbackData.productRequests.filter(
+      (feedback) => feedback.status === status
+    );
+  }
+
+  selectFeedbacksByStatus(status: ProductFeedbackStatus): void {
+    this.selectedFeedbackStatus.next(status);
+  }
+
+  getSelectedFeedbacksNumber(status: ProductFeedbackStatus | null) {
+    return this.filterFeedbacksByStatus(status).length;
+  }
+
+  getSelectedFeedbacksSubHeading(status: ProductFeedbackStatus | null): string {
+    switch (status) {
+      case 'planned':
+        return 'Features currently in research';
+      case 'in-progress':
+        return 'Features currently being developed';
+      case 'live':
+        return 'Features currently released';
+      default:
+        return 'Features currently suggested';
+    }
+  }
+}
