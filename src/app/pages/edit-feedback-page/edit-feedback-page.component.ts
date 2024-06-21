9832fb1f6223613828631157e4961f5e7689d8b7
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-06-21 15:14:07 +0000

    feat: implement custom select dropdown

--- src/app/pages/edit-feedback-page/edit-feedback-page.component.ts
+++ src/app/pages/edit-feedback-page/edit-feedback-page.component.ts
@@ -1,16 +1,57 @@
-import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
 
 @Component({
   selector: 'app-edit-feedback-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './edit-feedback-page.component.html',
 })
 export class EditFeedbackPageComponent {
+  @ViewChild('categorySelect') categorySelect!: ElementRef<HTMLSelectElement>;
+  @ViewChild('statusSelect') statusSelect!: ElementRef<HTMLSelectElement>;
+
+  categoryOptions = ['Feature', 'UI', 'UX', 'Enhancement', 'Bug'];
+  statusOptions = ['Suggestion', 'Planned', 'In-Progress', 'Live'];
+  selectedCategory = 'Feature';
+  selectedStatus = 'Suggestion';
+  isCategoryMenuOpen = false;
+  isStatusMenuOpen = false;
+  isDropdownOpen = false;
+
   constructor(private _location: Location) {}
 
   goBack(): void {
     this._location.back();
+  }
+
+  selectCategoryMenuOption(index: number): void {
+    const option = this.categoryOptions[index];
+    this.selectedCategory = option;
+    this.categorySelect.nativeElement.value = option.toLowerCase();
+  }
+
+  toggleCategoryMenuDropdown(isOpen: boolean): void {
+    this.isCategoryMenuOpen = isOpen;
+    this.isStatusMenuOpen = false;
+    this.isDropdownOpen = true;
+  }
+
+  selectStatusMenuOption(index: number): void {
+    const option = this.statusOptions[index];
+    this.selectedStatus = option;
+    this.statusSelect.nativeElement.value = option.toLowerCase();
+  }
+
+  toggleStatusMenuDropdown(isOpen: boolean): void {
+    this.isStatusMenuOpen = isOpen;
+    this.isCategoryMenuOpen = false;
+    this.isDropdownOpen = true;
+  }
+
+  closeDropdown(): void {
+    this.isDropdownOpen = false;
+    this.isStatusMenuOpen = false;
+    this.isCategoryMenuOpen = false;
   }
 }
--- src/app/pages/new-feedback-page/new-feedback-page.component.ts
+++ src/app/pages/new-feedback-page/new-feedback-page.component.ts
@@ -1,16 +1,31 @@
-import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-new-feedback-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './new-feedback-page.component.html',
 })
 export class NewFeedbackPageComponent {
+  @ViewChild('categorySelect') categorySelect!: ElementRef<HTMLSelectElement>;
+  categoryOptions = ['Feature', 'UI', 'UX', 'Enhancement', 'Bug'];
+  selectedCategory = 'Feature';
+  isMenuOpen = false;
+
   constructor(private _location: Location) {}
 
   goBack(): void {
     this._location.back();
+  }
+
+  selectMenuOption(index: number): void {
+    const option = this.categoryOptions[index];
+    this.selectedCategory = option;
+    this.categorySelect.nativeElement.value = option.toLocaleLowerCase();
+  }
+
+  toggleMenuDropdown(isOpen: boolean): void {
+    this.isMenuOpen = isOpen;
   }
 }
