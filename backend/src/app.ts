fd4afe0fd310f648ac5e0be721f09844140fb851
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-10-14 16:16:32 +0000

    feat: setup express server

---
+++ backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import cors from 'cors';
+
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+const enivronment = process.env.NODE_ENV ?? 'development';
+const envFile = enivronment === 'development' ? '.env.local' : '.env';
+
+dotenv.config({ path: path.join(__dirname, envFile) });
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+
+app.get('/', (_req, res) => {
+    res.send('Product Feedback API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Application Server is running on http://localhost:${PORT}`);
+});
