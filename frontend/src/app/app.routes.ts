926dfd5cedab0aa0d5069795bf13a1a11ccf7073
Prince T. Asamoah <prince.t.asamoah@gmail.com>
2024-10-15 14:34:54 +0000

    feat: create feedbacks route with child pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,17 +1,22 @@
 import { Routes } from '@angular/router';
-import { NewFeedbackPageComponent } from './pages/new-feedback-page/new-feedback-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { EditFeedbackPageComponent } from './pages/edit-feedback-page/edit-feedback-page.component';
-import { FeedbackDetailsPageComponent } from './pages/feedback-details-page/feedback-details-page.component';
-import { RoadmapPageComponent } from './pages/roadmap-page/roadmap-page.component';
+import { AddFeedbackPageComponent } from './features/feedback/pages/add-feedback-page/add-feedback-page.component';
+import { FeedbackHomePageComponent } from './features/feedback/pages/feedback-home-page/feedback-home-page.component';
+import { EditFeedbackPageComponent } from './features/feedback/pages/edit-feedback-page/edit-feedback-page.component';
+import { FeedbackDetailsPageComponent } from './features/feedback/pages/feedback-details-page/feedback-details-page.component';
+import { RoadmapPageComponent } from './features/feedback/pages/roadmap-page/roadmap-page.component';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardPageComponent },
-  { path: '', component: HomePageComponent },
-  { path: 'new-feedback', component: NewFeedbackPageComponent },
-  { path: 'edit-feedback', component: EditFeedbackPageComponent },
-  { path: 'feedback-details', component: FeedbackDetailsPageComponent },
-  { path: 'roadmap', component: RoadmapPageComponent },
+  {
+    path: 'feedbacks',
+    children: [
+      { path: '', component: FeedbackHomePageComponent },
+      { path: 'add', component: AddFeedbackPageComponent },
+      { path: 'edit', component: EditFeedbackPageComponent },
+      { path: 'details', component: FeedbackDetailsPageComponent },
+      { path: 'roadmap', component: RoadmapPageComponent },
+    ],
+  },
 ];
