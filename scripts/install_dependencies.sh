4618762acb8b1675fa8cda5c3b479a5680a37ca1
Vishesh Ghule <141129836+VisheshGhule@users.noreply.github.com>
2025-06-11 14:24:19 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# Navigate to backend folder and install dependencies
-cd /home/ubuntu/product-feedback-app/backend
-npm install
 
+# Load NVM manually (so npm/node are available)
+export NVM_DIR="/home/ubuntu/.nvm"
+source "$NVM_DIR/nvm.sh"
+
+# Use node version (just to be safe)
+nvm use 20
+
+# Go to the correct backend path and install deps
+cd /home/ubuntu/product-feedback-app/backend || exit
+npm install
