8c520924c6448019ebb18b51255cd9f87f8e1fb9
Vishesh Ghule <141129836+VisheshGhule@users.noreply.github.com>
2025-06-11 16:08:56 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Manually load Node.js and npm into PATH (bypass nvm limitations)
 export NVM_DIR="/home/ubuntu/.nvm"
 export PATH="$NVM_DIR/versions/node/v20.19.2/bin:$PATH"
 
 cd /home/ubuntu/product-feedback-app/backend || exit
 
-# Start the backend in the background and log output
 npm run start >> /home/ubuntu/server.log 2>&1 &
