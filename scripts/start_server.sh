35859129a11b5bfdb1d51f53b87d3ebef76548be
Vishesh Ghule <141129836+VisheshGhule@users.noreply.github.com>
2025-06-11 16:33:44 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Load NVM
 export NVM_DIR="/home/ubuntu/.nvm"
-export PATH="$NVM_DIR/versions/node/v20.19.2/bin:$PATH"
+source "$NVM_DIR/nvm.sh"
+nvm use 20
 
+# Go to backend folder
 cd /home/ubuntu/product-feedback-app/backend || exit
 
+# Start the server in background and log output
 npm run start >> /home/ubuntu/server.log 2>&1 &
