3d87ac3cf1dfc7ed1315e38ed22b9a39e6ede5ea
Vishesh Ghule <141129836+VisheshGhule@users.noreply.github.com>
2025-06-11 14:32:03 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# Navigate to backend folder and start the server
-cd /home/ubuntu/product-feedback-app/backend
-npm run start
 
+# Load NVM so npm is available
+export NVM_DIR="/home/ubuntu/.nvm"
+source "$NVM_DIR/nvm.sh"
+nvm use 20
+
+# Navigate to backend
+cd /home/ubuntu/product-feedback-app/backend || exit
+
+# Start the server
+npm run start
