6a67a2d52888006260cfdad9848d4bb650728021
Vishesh <visheshghule2@gmail.com>
2025-06-11 10:18:37 +0530

    Add deployment scripts and appspec.yml

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Navigate to backend folder and install dependencies
+cd /home/ec2-user/product-feedback-app/backend
+npm install
+
---
+++ scripts/start_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Navigate to backend folder and start the server
+cd /home/ec2-user/product-feedback-app/backend
+npm run start
+
