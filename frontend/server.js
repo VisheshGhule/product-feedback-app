c1e0f5c689f4ee7dbdb2df3bd9b66caa66b33295
Vishesh Ghule <141129836+VisheshGhule@users.noreply.github.com>
2025-06-21 12:23:33 +0530

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 4200;
-const DIST_DIR = path.join(__dirname, 'dist/product-feedback-app/browser');
 
+// Serve static files
+const DIST_DIR = path.join(__dirname, 'dist/product-feedback-app/browser');
 app.use(express.static(DIST_DIR));
 
+// Redirect all routes to index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(DIST_DIR, 'index.html'));
 });
