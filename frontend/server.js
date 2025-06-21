1e8c62aa88e586f172ebb6410966ca966f273cb7
Vishesh Ghule <141129836+VisheshGhule@users.noreply.github.com>
2025-06-21 17:29:05 +0530

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,16 +1,1 @@
-const path = require('path');
-const express = require('express');
-const app = express();
 
-const PORT = process.env.PORT || 4200;
-const DIST_DIR = path.join(__dirname, 'dist/product-feedback-app/browser');
-
-app.use(express.static(DIST_DIR));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(DIST_DIR, 'index.html'));
-});
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Frontend running at http://localhost:${PORT}`);
-});
