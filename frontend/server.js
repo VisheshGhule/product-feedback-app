ea56dc4adb8473a8b74268284d689a4855bd5029
Vishesh Ghule <141129836+VisheshGhule@users.noreply.github.com>
2025-06-21 11:38:49 +0530

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -9,8 +9,8 @@ const DIST_DIR = path.join(__dirname, 'dist');
 
 app.use(express.static(DIST_DIR));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(DIST_DIR, 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(DIST_DIR, 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
