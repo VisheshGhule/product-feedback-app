785392507dc8a579b4109d9351255febed838050
Vishesh Ghule <141129836+VisheshGhule@users.noreply.github.com>
2025-06-21 13:20:06 +0530

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 4200;
-const DIST_DIR = path.join(__dirname, 'dist/app');
+const DIST_DIR = path.join(__dirname, 'dist/app'); // <-- Updated path
 
 app.use(express.static(DIST_DIR));
 
